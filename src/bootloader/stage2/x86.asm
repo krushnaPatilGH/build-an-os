7af704953374b53cfb371a1af4455f5f33b9433a
krushnaPatilGH <krushna.patil.ga@gmail.com>
2024-07-08 18:23:12 +0530

    video 5

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,6 +2,41 @@ bits 16
 
 section _TEXT class=CODE
 
+; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* reminder);
+global _x86_div64_32
+_x86_div64_32:
+    push bp                 ; save old call frame
+    mov bp, sp              ; initialize new call frame
+
+    push bx
+
+    ; divide upper 32 bits
+    mov eax, [ bp + 8  ]    ; eax dividend => upper 32 bits  
+    mov ecx, [ bp + 12 ]    ; ecx divisor 
+    xor edx, edx
+    div ecx                 ; eax => quotient, edx => reminder
+
+    ; store upper 32 bits of quotient
+    mov bx, [ bp +  16 ]
+    mov [ bx + 4 ], eax 
+
+    ; divide lower 32 bits
+    mov eax, [ bp + 4 ]
+    div ecx
+
+    ; store results
+    mov [ bx ], eax
+    mov bx, [ bp + 18 ]
+    mov [ bx ], edx
+
+    pop bx
+
+    ; restore old call frame
+    mov sp, bp 
+    pop bp
+    ret
+
+
 global _x86_Video_WriteCharTeletype
 _x86_Video_WriteCharTeletype:
     push bp                 ; save old call frame
